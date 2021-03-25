12a5dcf17c0329bb088fb8e3750f888bf9f3499d
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-25 14:48:20 +0000

    JS ERRORS

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-let age = 15
+const age = 15
 let guess = 0
 
 while (guess != age) {
  guess = prompt (' Guess My Age! ')
  if (age > guess) { 
   alert ('Way to Low!') 
- } else if (guess < 100) { 
+ } else if (guess > 15) { 
   alert (' heyyyy im not that old!') 
- } else if (guess >= 100)  {
-  alert ('hey way to high!')
- }
+ } 
 }
 alert ('You Got it!')
 document.write ('CONGRATULATIONS YOU WIN!')
