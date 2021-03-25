b5547960079fa6b5cc9831c95ba5c46d0eccd228
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-25 15:08:38 +0000

    Fixed errors

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const age = 15
 let guess = 0
 
-while (guess != age) { 
-  guess = prompt(' Guess My Age! ')
+while (guess !== age) { 
+  guess = prompt(' Guess My Age! ') 
+  guess = parseInt(guess)
   if (age > guess) {
     alert('Way to Low!')
  }  else if (guess > 15) {
