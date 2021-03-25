784d8c922c8cd522f0f25ae9b722c652e6ba43e9
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-25 16:21:45 +0000

    Added comments

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const age = 15
 let guess = 0
+// These are the variables for the guess and the age
 
 while (guess !== age) {
   guess = prompt(' Guess My Age! ')
@@ -9,6 +10,8 @@ while (guess !== age) {
   } else if (guess > 15) {
     alert(' heyyyy im not that old!')
   }
+  // These are what happens with what you input if its to high or to low, and the promp that pops up, and what its supposed to say if youre incorrect
 }
 alert('You Got it!')
 document.write('CONGRATULATIONS YOU WIN!')
+// These are alerts and what it says if you guess my age correct
