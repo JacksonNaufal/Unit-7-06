7780def8186d2f8baf9ab38eff0eb99be3535976
Jackson Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-25 15:06:21 +0000

    Spaces

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let guess = 0
 
 while (guess != age) { 
   guess = prompt(' Guess My Age! ')
- if (age > guess) {
-  alert('Way to Low!')
- } else if (guess > 15) {
-  alert(' heyyyy im not that old!')
- }
+  if (age > guess) {
+    alert('Way to Low!')
+ }  else if (guess > 15) {
+    alert(' heyyyy im not that old!')
+  }
 }
 alert('You Got it!')
 document.write('CONGRATULATIONS YOU WIN!')
